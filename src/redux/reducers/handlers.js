fa34b68add59180eb7a9391bcbde1b2a318b3adc
user <user@Mac15.local>
2018-07-25 14:54:25 +0700

    - add Storybook

---
+++ src/redux/reducers/handlers.js
@@ -0,0 +1,32 @@
+/*
+export const constants = {
+  SEARCHED_FILM: 'SEARCHED_FILM',
+  SEARCH_FILM_SUCCEDED: 'SEARCH_FILM_SUCCEDED',
+  SEARCH_FILM_FAILED: 'SEARCH_FILM_FAILED',
+  FETCHED_FILM: 'FETCHED_FILM'
+};
+
+export default function createReducer(initialState, handlers) {
+  return (state = initialState, action) => {
+    const handler = handlers[action.type];
+    if (!handler) {
+      return state;
+    }
+    return handler({ ...state }, { ...action.payload });
+  };
+}
+
+export default function createReducer(initialState, handlers) {
+  return (state = initialState, action) =>
+    handlers[action.type]
+    ? handlers[actions.type](state, action)
+    : state;
+*/
+
+/*
+const handlers = [
+  {},
+  {},
+  {}
+]
+*/
--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,6 +1,17 @@
 import { constants as C } from '../actions/constants';
 import { initialState } from './initialState';
 
+/*
+export default function createReducer(initialState, handlers) {
+  return (state = initialState, action) => {
+    const handler = handlers[action.type];
+    if (!handler) {
+      return state;
+    }
+    return handler({ ...state }, { ...action.payload });
+  };
+}
+*/
 
 export function films(state = initialState, action) {
   switch (action.type) {
