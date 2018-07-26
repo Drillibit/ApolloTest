d1b62a7dddd17271cf3b79b6e428ac30e322fa5f
Daler <dmadaminov@allmax.team>
2018-07-26 15:58:57 +0700

    remove css, scss and example files

--- src/application.jsx
+++ src/application.jsx
@@ -2,21 +2,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-import './assets/css/styles.css';
-import './assets/scss/styles.scss';
-
 /**
  *  containers imports
  */
-import { BannerContainer } from './containers/BannerContainer';
 import { FilmsListContainer } from './containers/FilmsListContainer';
 import { SearchFilmContainer } from './containers/SearchFilm';
 
 export const Application = hot(module)(() => (
   <React.Fragment>
-    <h1 className="title">React Starter Template</h1>
-    <p>Build with Webpack 4</p>
-    <BannerContainer />
     <SearchFilmContainer />
     <FilmsListContainer />
   </React.Fragment>
