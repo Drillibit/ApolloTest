f19abdd5c0c3005095b5d966385d23d7fef5a215
Valentin <valentin@Mac36.local>
2018-07-31 12:05:00 +0700

    Added imports

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { faHeart, faChevronDown, faChevronLeft, faCheck, faSearch, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartFill } from '@fortawesome/free-regular-svg-icons';
+import { faFacebook, faGoogle, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import './UIKit/Buttons';
 import '../src/components/injectGlobalStyles';
 
 
-library.add(fas, far);
+library.add(faHeart, faChevronDown, faChevronLeft, faCheck, faSearch, faPlay, faHeartFill, faFacebook, faGoogle, faTwitter);
