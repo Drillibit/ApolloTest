c1235a48e4ee35bace0de43023cb893a1aa98aa7
Valentin <valentin@Mac36.local>
2018-07-31 09:50:22 +0700

    Rebuilded preloader according to react-way

--- src/components/UIKit/Preloader.jsx
+++ src/components/UIKit/Preloader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import PropTypes, { func } from 'prop-types';
 
-
-const Preload = styled.div`
+const keyframes = css`
   @keyframes change-height-1{
     50% {
       height: 50px;
@@ -23,71 +23,90 @@ const Preload = styled.div`
       height: 65px;
     }
   }
-  
-  .rectangle-wr {
-    height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 50px;
-    margin-bottom: 28px;
+`;
 
-    .rectangle {
-      border-radius: 15px;
-      background-color: #ff0079;
-      width: 10px;
-      margin-left: 8px;
-    }
+const RectangleWr = styled.div`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 28px;
+`;
 
-    .rectangle-1 {
-      height: 24px;
-      animation: change-height-1 1s ease infinite;
-    }
-    .rectangle-2 {
-      height: 38px;
-      animation: change-height-2 1s 0.5s ease infinite;
-    }
-    .rectangle-3 {
-      height: 50px;
-      animation: change-height-3 0.8s ease infinite;
-    }
-    .rectangle-4 {
-      height: 34px;
-      animation: change-height-4 0.8s ease infinite;
-    }
-    .rectangle-5 {
-      height: 50px;
-      animation: change-height-3 0.8s 0.5s ease infinite;
-    }
-    .rectangle-6 {
-      height: 34px;
-      animation: change-height-4 0.8s 0.5s ease infinite;
-    }
-  }
+const MessageWr = styled.div`
+  display: block;
+  text-align: center;
+  height: 38px;
+  color: #babbc3;
+  font-size: 30px;
+  font-weight: 600;
+  line-height: normal;
+  font-family: 'Source Sans Pro', sans-serif;
+  text-indent: 10px;
+`;
 
-  .message-wr {
-    display: block;
-    text-align: center;
-    height: 38px;
-    color: #babbc3;
-    font-size: 30px;
-    font-weight: 600;
-    line-height: normal;
-    font-family: 'Source Sans Pro', sans-serif;
-    text-indent: 10px;
-  }
+const Rectangle = styled.div`
+  border-radius: 15px;
+  background-color: #ff0079;
+  width: 10px;
+  margin-left: 8px;
+`;
+
+const RectangleOne = Rectangle.extend`
+  height: 24px;
+  animation: change-height-1 1s ease infinite;
+`;
+
+const RectangleTwo = Rectangle.extend`
+  height: 38px;
+  animation: change-height-2 1s 0.5s ease infinite;
+`;
+
+const RectangleThree = Rectangle.extend`
+  height: 50px;
+  animation: change-height-3 0.8s ease infinite;
+`;
+
+const RectangleFour = Rectangle.extend`
+  height: 34px;
+  animation: change-height-4 0.8s ease infinite;
+`;
+
+const RectangleFive = Rectangle.extend`
+  height: 50px;
+  animation: change-height-3 0.8s 0.5s ease infinite;
+`;
+
+const RectangleSix = Rectangle.extend`
+  height: 34px;
+  animation: change-height-4 0.8s 0.5s ease infinite;
+`;
+
+const Preload = styled.div`
+  ${keyframes};
 `;
 
 export const Preloader = ({ onClick, children }) => (
   <Preload onClick={onClick}>
-    <div className="rectangle-wr">
-      <div className="rectangle rectangle-1" />
-      <div className="rectangle rectangle-2" />
-      <div className="rectangle rectangle-3" />
-      <div className="rectangle rectangle-4" />
-      <div className="rectangle rectangle-5" />
-      <div className="rectangle rectangle-6" />
-    </div>
-    <span className="message-wr">{children}</span>
+    <RectangleWr>
+      <RectangleOne />
+      <RectangleTwo />
+      <RectangleThree />
+      <RectangleFour />
+      <RectangleFive />
+      <RectangleSix />
+    </RectangleWr>
+    <MessageWr>{children}</MessageWr>
   </Preload>
 );
+
+
+Preloader.propTypes = {
+  onClick: func,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]).isRequired
+};
+
+Preloader.defaultProps = {
+  onClick: f => f,
+};
