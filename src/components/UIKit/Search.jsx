4a8578887faf036a616364f8034cfd98f1ee7b0d
user <user@Mac15.local>
2018-07-31 17:22:31 +0700

    search storybook updated

--- src/components/UIKit/Search.jsx
+++ src/components/UIKit/Search.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import { func } from 'prop-types';
 import styled from 'styled-components';
 
-// test films list
-// отключить filmList когда будет бекэнд
-import { filmsList } from '../Search/testFilmList';
-
 const searchPhrase = 'Найти по названию, жанру, актеру';
 
-const StyledForm = styled.form`
-  
-`;
-
-const StyledInput = styled.input`
+const InputStyled = styled.input`
   width: 400px;
-  height: 64px;
+  height: 32px;
+  padding: 16px 24px;
   border-radius: 5px;
   background-color: #ffffff;
   box-shadow: 0 2px 7px 0 rgba(0, 0, 0, 0.1);
@@ -27,53 +20,64 @@ const StyledInput = styled.input`
   color: #494c62;
 `;
 
-const StyledOption = styled.option`
+const UlStyled = styled.ul`
+  padding: 0;
+  margin: 2px;
   width: 500px;
-  height: 101px;
-  border-radius: 8px;
+  border-radius: 2px;
   background-color: #ffffff;
   box-shadow: 0 12px 75px 2px rgba(0, 0, 0, 0.41), 0 2px 9px 1px rgba(0, 0, 0, 0.28);
-  font-size: 40px;
+`;
+
+const LiStyled = styled.li`
+  margin: 5px 0;
+  padding-left: 24px;
+  cursor: pointer;
+  height: 25px;
+  font-size: 20px;
   font-weight: normal;
   font-style: normal;
   font-stretch: normal;
   line-height: normal;
   letter-spacing: normal;
   color: #494c62;
-  border: 10px;
+  list-style-type: none;
 `;
 
-const StyledDataList = styled.datalist`
-  width: 500px;
-  height: 101px;
-  border-radius: 8px;
-  background-color: #ffffff;
-  box-shadow: 0 12px 75px 2px rgba(0, 0, 0, 0.41), 0 2px 9px 1px rgba(0, 0, 0, 0.28);
-  font-size: 40px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #494c62;
-  border: 10px;
+const A = styled.a`
+  &:hover a {
+    color: #ff0079;
+    text-decoration: underline;
+  };
+  text-decoration: none;
 `;
 
-export const Search = ({ search }) => (
-  <form>
-    <StyledInput type="text" onChange={search} placeholder={searchPhrase} list="search" />
-    <StyledDataList id="search">
-      {
-        filmsList.map(item => <StyledOption value={item.title} key={item.id} />)
-      }
-    </StyledDataList>
-  </form>
-);
+
+// в гугле реализовано через ul > li
+export const Search = ({ filmsList, onChange }) => {
+  // console.log(filmsList, 'filmsList');
+  // console.log(searchFilm, 'searchFilm');
+  return (
+    <form>
+      <InputStyled
+        maxLength={37}
+        type="text"
+        placeholder={searchPhrase}
+        onChange={onChange}
+      />
+      <UlStyled>
+        {
+          filmsList.map(film => <LiStyled key={film.id}><A href="">{film.title}</A></LiStyled>)
+        }
+      </UlStyled>
+    </form>
+  );
+};
 
 Search.propTypes = {
-  search: func,
+  onChange: func
 };
 
 Search.defaultProps = {
-  search: f => f,
+  onChange: f => f,
 };
--- stories/UIKit/Search.jsx
+++ stories/UIKit/Search.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
+// test films list
+// отключить filmList когда будет бекэнд
+import { filmsList } from '../../src/components/Search/testFilmList';
+
+
 import { Search } from '../../src/components/UIKit/Search';
 
 const stories = storiesOf('UIKit/Search', module);
 
 stories.addWithJSX('Search', () => (
-  <Search onChange={action('search')}>Search</Search>
+  <Search filmsList={filmsList} onChange={action('onChange')} />
 ));
