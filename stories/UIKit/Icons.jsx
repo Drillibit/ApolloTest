5aa472272bde66d99b3a40aae739ed77d7e7a4d8
Valentin <valentin@Mac36.local>
2018-08-01 12:55:21 +0700

    Added description from icons

--- stories/UIKit/Icons.jsx
+++ stories/UIKit/Icons.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Icon } from '../../src/components/UIKit/Icon';
+import { Container } from '../helpers/Container';
 
 const stories = storiesOf('UIKit/Icons', module);
 
 stories.addWithJSX('basic', () => (
-  <React.Fragment>
-    <Icon icon="heart" />
-    <Icon icon="chevron-left" />
-    <Icon icon="chevron-down" />
-    <Icon icon="chevron-up" />
-    <Icon icon="check" />
-    <Icon icon="search" />
-    <Icon icon="play" />
-    <Icon icon="star" />
-    <Icon icon="heart-fill" />
-    <Icon icon="facebook" />
-    <Icon icon="twitter" />
-    <Icon icon="google-plus" />
-  </React.Fragment>
+  <Container>
+    <Icon icon="heart" /> heart
+    <Icon icon="chevron-left" /> chevron-left
+    <Icon icon="chevron-down" /> chevron-down
+    <Icon icon="chevron-up" /> chevron-up
+    <Icon icon="check" /> check
+    <Icon icon="search" /> search
+    <Icon icon="play" /> play
+    <Icon icon="star" /> star
+    <Icon icon="heart-fill" /> heart-fill
+    <Icon icon="facebook" /> facebook
+    <Icon icon="twitter" /> twitter
+    <Icon icon="google-plus" /> google-plus
+  </Container>
 ));
 
