78ea057c22113b7ace955da5eccb0abe94c603b4
user <user@Mac15.local>
2018-08-01 13:17:57 +0700

    wrap Quote storybook in Component

--- stories/UIKit/Quote.jsx
+++ stories/UIKit/Quote.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Quote } from '../../src/components/UIKit/Quote';
+import { Container } from '../helpers/Container';
 
 const stories = storiesOf('UIKit/Quote', module);
 
 const someText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la';
 
 stories.addWithJSX('Quote', () => (
-  <Quote tagline={someText} />
+  <Container>
+    <Quote tagline={someText} />
+  </Container>
 ));
