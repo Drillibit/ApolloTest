d8ea15f9eb43595a87e1c91f56ecdd95faa411df
Daler <dmadaminov@allmax.team>
2018-08-01 12:45:05 +0700

    resolve merge conflict

--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import './UIKit/Container';
-=======
-import './UIKit/Buttons';
 import './UIKit/Preloaders';
 import '../src/components/injectGlobalStyles';
 import './UIKit/Typography/HeadingTags';
 import './UIKit/Typography/TextFields';
->>>>>>> master
