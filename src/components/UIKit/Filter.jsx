2c7193b0eedc49715c5c1ffdc90cd63300e2791a
Valentin Pokatskii <vpokatskii@allmax.team>
2018-08-02 11:28:06 +0700

    init

---
+++ src/components/UIKit/Filter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const FilterList = styled.div``;
+
+export const Filter = () => (
+  <FilterList>
+    123
+  </FilterList>
+);
