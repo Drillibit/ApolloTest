d3e8c8327c1a2e41c4ba77c11ef25fc7fdbb63fb
user <user@Mac15.local>
2018-08-07 16:24:25 +0700

    delete styled import

--- src/components/UIKit/Logo.jsx
+++ src/components/UIKit/Logo.jsx
@@ -4,13 +4,9 @@ import { string } from 'prop-types';
 
 import { Keeno } from '../../assets/img/keeNo';
 
-const ImgStyled = styled(Keeno)`
-  display: block;
-  width: 100%;
-`;
 
 export const Logo = ({ color }) => (
-  <ImgStyled color={color} />
+  <Keeno color={color} />
 );
 
 Logo.propTypes = {
