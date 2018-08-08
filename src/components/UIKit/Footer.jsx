fe74c07aebe474a96a7f81151b7b964012bcb665
user <user@Mac15.local>
2018-08-08 10:16:54 +0700

    add styles in footer component

--- src/components/UIKit/Footer.jsx
+++ src/components/UIKit/Footer.jsx
@@ -7,7 +7,14 @@ import { SmallText } from './Typography';
 
 const FooterStyled = styled.footer`
   color: #babbc3;
-  text-align: center
+  background-color: #fff;
+  box-shadow: 0 -1px 4px 0 rgba(0, 0, 0, 0.08);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 114px;
+  width: 100%;
 `;
 
 export const Footer = ({ text, color }) => (
