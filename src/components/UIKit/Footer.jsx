ee09e1abd7380dd01afb8d9fee77820c9f65772e
user <user@Mac15.local>
2018-08-08 15:14:49 +0700

    update from master

--- src/components/UIKit/Footer.jsx
+++ src/components/UIKit/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { string } from 'prop-types';
+// import { string } from 'prop-types';
 import styled from 'styled-components';
 
 import { Logo } from './Logo';
-import { SmallText } from './Typography';
 
 const FooterStyled = styled.footer`
   color: #babbc3;
@@ -17,19 +16,9 @@ const FooterStyled = styled.footer`
   width: 100%;
 `;
 
-export const Footer = ({ text, color }) => (
+export const Footer = () => (
   <FooterStyled>
-    <Logo color={color} />
-    <SmallText>{text}</SmallText>
+    <Logo />
+    {console.log(this)}
   </FooterStyled>
 );
-
-Footer.propTypes = {
-  text: string,
-  color: string
-};
-
-Footer.defaultProps = {
-  text: 'Hello world',
-  color: '#ffffff'
-};
--- stories/UIKit/Footer.jsx
+++ stories/UIKit/Footer.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { oneOfType, node, arrayOf, string } from 'prop-types';
 import { storiesOf } from '@storybook/react';
 
 import { Footer } from '$components/UIKit/Footer';
 import { colors } from '$components/helpers/colors';
+import { SmallText } from '$components/UIKit/Typography';
 import { Container } from '../helpers/Container';
 
-
 const stories = storiesOf('UIKit/Footer', module);
-const text = '2018, Все права защищены';
+//const text = '2018, Все права защищены';
+
+const FooterWrapper = ({ text }) => (
+  <Footer>
+    <SmallText>{text}</SmallText>
+  </Footer>
+);
 
 stories.addWithJSX('Footer', () => (
   <Container>
-    <Footer text={text} color={colors.grey500} />
+    <FooterWrapper />
   </Container>
 ));
+
+FooterWrapper.propTypes = {
+  text: string
+};
+
+FooterWrapper.defaultProps = {
+  text: '2018, Все права защищены'
+};
