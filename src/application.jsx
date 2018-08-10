4f2d33fb5aad6cffb9e336939b28c6216498ce74
Baranov Ivan <ibaranov@allmax.team>
2018-08-10 12:20:43 +0700

    fix app

--- src/application.jsx
+++ src/application.jsx
@@ -7,17 +7,12 @@ import { hot } from 'react-hot-loader';
  */
 import { FilmsListContainer } from './containers/FilmsListContainer';
 import { SearchFilmContainer } from './containers/SearchFilm';
-<<<<<<< HEAD
-import { Modal } from './components/UIKit/ModalInit';
-=======
 import { SearchContainer } from './containers/SearchContainer';
->>>>>>> master
 
 export const Application = hot(module)(() => (
   <React.Fragment>
     <SearchContainer />
     <SearchFilmContainer />
-    <Modal />
     <FilmsListContainer />
   </React.Fragment>
 ));
