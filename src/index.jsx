b9045fb636e2e68f015d8cb71349737120684a09
Valentin Pokatskii <vpokatskii@allmax.team>
2018-08-13 16:46:20 +0700

    deleted TROLLING spaces

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,7 @@ import { render } from 'react-dom';
 
 import { Application } from './application';
 
-
 render(
   <Application />,
   document.getElementById('root')
 );
-
--- src/redux/store.js
+++ src/redux/store.js
@@ -22,4 +22,3 @@ export const store = createStore(
 );
 
 sagaMiddleware.run(rootSaga);
-
