bef7e81becf480d9ceb76c5a808d09b5eb3b8b15
user <user@Mac15.local>
2018-08-13 16:21:58 +0700

    delete import

--- stories/UIKit/FeaturedMovie.jsx
+++ stories/UIKit/FeaturedMovie.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Grid, Col, Row } from 'react-styled-flexboxgrid';
