90a291f1ff282143df4d6a995baa444dd6b6b186
user <user@Mac15.local>
2018-08-13 12:45:04 +0700

    add Grid

--- stories/UIKit/FeaturedMovie.jsx
+++ stories/UIKit/FeaturedMovie.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { Grid, Col, Row } from 'react-styled-flexboxgrid';
 
 import { Rating } from '$components/UIKit/Rating';
-import { Button } from '$components/UIKit/Button';
+import { StyledButton } from '$components/UIKit/Button';
 import { Icon } from '$components/UIKit/Icon';
 import { LargeText, H1 } from '$components/UIKit/Typography';
 import { Container } from '../helpers/Container';
@@ -37,108 +38,94 @@ const somefilm = {
   // не существующее свойство! заменить как появится сервер!
   "timing": 133
 };
+
+const theme = {
+  'flexboxgrid': {
+    'outerMargin': 0,
+    'gutterWidth': 0,
+  }
+};
 /* eslint-enable */
 
 const FeaturedMovieStyled = styled.div`
-  width: 1437px;
-  height: 700px;
-  background: #000 url(https://image.tmdb.org/t/p/original/3s9O5af2xWKWR5JzP2iJZpZeQQg.jpg) no-repeat;
+  position: relative;
+  min-height:700px;
+  background: #000 url(https://image.tmdb.org/t/p/original/3s9O5af2xWKWR5JzP2iJZpZeQQg.jpg) no-repeat center center;
   background-size: cover;
-  display: flex;
-  justify-content: center;
+  padding: 20px;
   align-items: flex-end;
-`;
-
-const WrapBlock = styled.div`
-  width: 1300px;
-  padding: 40px;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const FilmTitleBlock = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
   color: #fff;
 `;
 
-const GenresBlock = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+const WrapButton = StyledButton.extend`
+  margin: 10px 40px 10px 0;
 `;
 
-const Genres = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  & span {
-    margin-right: 10px;
-  }
-`;
-
-const DurationBlock = styled.div`
+const Timing = styled.div`
   border-left: 2px solid white;
   opacity: 0.87;
   border-radius: 2px;
   padding-left: 20px;
-  margin-left: 10px;
 `;
 
-const FilmButtonsBlock = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: no-wrap;
-  align-items: center;
-  align-content: center;
-  justify-content: space-between;
+const Genres = styled.div`
+  word-spacing: 10px;
 `;
 
-const RatingBlock = styled.div`
-  display: flex;
-  align-content: center;
-
-  justify-content: center;
-  
-  padding: 16px;
+const RatingStyled = styled.div`
+  height: 48px;
+  @media screen and (min-width: 1060px) {
+    position: absolute; 
+    right: 20px;
+    bottom: 20px;
+  };
+  margin: 10px 0 10px 0;
+  padding: 10px;
   border-radius: 5px;
   border: solid 2px rgba(255, 255, 255, 0.2);
   background-color: rgba(73, 76, 98, 0.6);
 `;
 
-const ButtonsWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  & button {
-    margin-right: 20px; 
-  }
-`;
-
 stories.addWithJSX('FeaturedMovie', () => (
   <Container dark>
     <FeaturedMovieStyled>
-      <WrapBlock>
-        <FilmTitleBlock>
-          <LargeText><strong>СЕЙЧАС В КИНО</strong></LargeText>
-          <H1>{somefilm.title}</H1>
-          <GenresBlock>
-            <Genres>{searchGenre(somefilm.genre_ids).map(
-              (genre, index) => <span key={index}>{genre}&nbsp;</span>)}
-            </Genres>
-            <DurationBlock>{somefilm.timing} минуты</DurationBlock>
-          </GenresBlock>
-        </FilmTitleBlock>
+      <ThemeProvider theme={theme}>
+        <Grid bottom="xs">
 
-        <FilmButtonsBlock>
-          <ButtonsWrap>
-            <Button btnType="primary" btnSize="big" onClick={action('click')}>Подробнее</Button>
-            <Button btnType="transparent-white" btnSize="small" onClick={action('click')}><Icon icon="heart" />В избранное</Button>
-          </ButtonsWrap>
-          <RatingBlock><Rating voteAverage={somefilm.vote_average} voteCount={somefilm.vote_count} size="lg" /></RatingBlock>
-        </FilmButtonsBlock>
-      </WrapBlock>
+          <Row>
+            <Row start="xs">
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <LargeText><strong>СЕЙЧАС В КИНО</strong></LargeText>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={12}><H1>{somefilm.title}</H1></Col>
+              <Col xs={12} sm={12} md={3} lg={3}>
+                <Genres>{searchGenre(somefilm.genre_ids).map(
+                  (genre, index) => <span key={index}>{genre}&nbsp;</span>)}
+                </Genres>
+              </Col>
+              <Col xs={12} sm={12} md={3} lg={3}><Timing>{somefilm.timing} минуты</Timing></Col>
+            </Row>
+
+          </Row>
+
+
+          <Row middle="xs">
+
+            <Row middle="xs">
+              <Col xs={12} sm={6}><WrapButton btnType="primary" btnSize="big" onClick={action('click')}>Подробнее</WrapButton></Col>
+              <Col xs={12} sm={6}><WrapButton btnType="transparent-white" btnSize="small" onClick={action('click')}><Icon icon="heart" />В избранное</WrapButton></Col>
+            </Row>
+
+            <Row middle="xs">
+              <Col xs={12} sm={12} md={12} lg={12} >
+                <RatingStyled><Rating voteAverage={somefilm.vote_average} voteCount={somefilm.vote_count} size="lg" /></RatingStyled>
+              </Col>
+            </Row>
+
+          </Row>
+        </Grid>
+      </ThemeProvider>
     </FeaturedMovieStyled>
   </Container>
 ));
