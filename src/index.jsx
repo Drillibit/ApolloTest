17a86a77bd37097831dcedcbb7f5c01589092129
Valentin Pokatskii <vpokatskii@allmax.team>
2018-08-13 16:28:16 +0700

    replaced connects to store.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { Route } from 'react-router';
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createBrowserHistory } from 'history';
-import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router';
 
-import { reducers } from './redux/reducers';
-import { sagas } from './redux/movies/sagas';
 import { Application } from './application';
-import './components/helpers/injectGlobalStyles';
-
-const history = createBrowserHistory();
-
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  connectRouter(history)(reducers), // new root reducer with router state
-  composeWithDevTools(
-    applyMiddleware(
-      routerMiddleware(history), // for dispatching history actions
-      sagaMiddleware
-    ),
-  ),
-);
-
-sagaMiddleware.run(sagas);
-
-console.log(store);
 
 render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}> { /* place ConnectedRouter under Provider */ }
-      <div> { /* your usual react-router v4 routing */ }
-        <Route exact path="/" component={Application} />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
+  <Application />,
   document.getElementById('root')
 );
-
