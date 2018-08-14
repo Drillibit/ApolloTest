a28884574947b02e786ebe5c2cf4f01dd8092085
Baranov Ivan <ibaranov@allmax.team>
2018-08-14 16:38:08 +0700

    move absolute paths up

--- src/application.jsx
+++ src/application.jsx
@@ -4,12 +4,11 @@ import { hot } from 'react-hot-loader';
 import { Route } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
 import { library } from '@fortawesome/fontawesome-svg-core';
-
-
-import { ConnectedHeader } from './containers/ConnectedHeader';
 import { faHeart, faChevronUp, faChevronDown, faChevronLeft, faChevronRight, faCheck, faSearch, faPlay, faStar as faStarFill, faQuoteLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartFill, faStar } from '@fortawesome/free-regular-svg-icons';
 import { faFacebookF, faGooglePlusG, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+import { ConnectedHeader } from './containers/ConnectedHeader';
 
 import { store, history } from './redux/store';
 import { Main } from './components/Main';
