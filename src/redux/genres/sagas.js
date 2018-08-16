19e89db980027685c349ecb49d0890ac5b53a1f8
Valentin Pokatskii <vpokatskii@allmax.team>
2018-08-16 15:37:08 +0700

    added reducers and sagas

---
+++ src/redux/genres/sagas.js
@@ -0,0 +1,16 @@
+import { put, call, takeLatest } from 'redux-saga/effects';
+
+import * as CONSTANTS from './constants';
+import { setGenres } from './actions';
+import { requestFetchGenres } from './requests';
+
+function* fetchGenres() {
+  const { data } = yield call(requestFetchGenres);
+  if (data && data.genres) {
+    yield put(setGenres(data.genres));
+  }
+}
+
+export function* sagas() {
+  yield takeLatest(CONSTANTS.FETCH_NOW_PLAYING, fetchGenres);
+}
--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,11 @@
 import { fork } from 'redux-saga/effects';
 
 import { sagas as movies } from './movies/sagas';
+import { sagas as genres } from './genres/sagas';
 
 export function* rootSaga() {
-  yield fork(movies);
+  yield [
+    fork(movies),
+    fork(genres)
+  ];
 }
