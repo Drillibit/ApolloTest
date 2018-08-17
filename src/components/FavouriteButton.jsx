58bdee492b27417c9fc5c5e0fc1e93cfcdaf615d
Valentin Pokatskii <vpokatskii@allmax.team>
2018-08-17 09:38:10 +0700

    Added propTypes from FavouriteButton

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes, { func } from 'prop-types';
+
 import { Button } from '$UIKit/Button';
 import { Icon } from '$UIKit/Icon';
 import { colors } from '$helpers/colors';
@@ -8,3 +10,15 @@ export const FavouriteButton = ({ isFavourite, btnType, toggleFavourite }) => (
     <Icon icon={isFavourite ? 'heart-fill' : 'heart'} color={isFavourite ? colors.purple : 'inherit'} /> {isFavourite ? 'В избранном' : 'В избанное'}
   </Button>
 );
+
+FavouriteButton.propTypes = {
+  isFavourite: PropTypes.bool,
+  btnType: PropTypes.string,
+  toggleFavourite: func,
+};
+
+FavouriteButton.defaultProps = {
+  isFavourite: true,
+  btnType: '',
+  toggleFavourite: f => f,
+};
