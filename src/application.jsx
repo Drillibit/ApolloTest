4c1c4aaa87fa9a2d47c8befbb5896028d2200bca
Baranov Ivan <ibaranov@allmax.team>
2018-08-20 17:27:35 +0700

    persist initial setup

--- src/application.jsx
+++ src/application.jsx
@@ -6,16 +6,20 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHeart, faChevronUp, faChevronDown, faChevronLeft, faChevronRight, faCheck, faSearch, faPlay, faStar as faStarFill, faQuoteLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartFill, faStar } from '@fortawesome/free-regular-svg-icons';
 import { faFacebookF, faGooglePlusG, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, history } from './redux/store';
+import { STORE, PERSISTOR, history } from './redux/store';
 import { Main } from './components/Main';
+import { Preloader } from './components/UIKit/Preloader';
 import './components/helpers/injectGlobalStyles';
 
 export const Application = hot(module)(() => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Main />
-    </ConnectedRouter>
+  <Provider store={STORE}>
+    <PersistGate loading={<Preloader>Загрузка</Preloader>} persistor={PERSISTOR}>
+      <ConnectedRouter history={history}>
+        <Main />
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>
 ));
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,28 @@ import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createBrowserHistory } from 'history';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import storage from 'redux-persist/lib/storage';
 
 import { reducers } from './reducers';
 import { rootSaga } from './rootSaga';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['favourites'],
+  stateReconciler: autoMergeLevel2
+};
 
 export const history = createBrowserHistory();
 
 const sagaMiddleware = createSagaMiddleware();
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = createStore(
-  connectRouter(history)(reducers),
+  connectRouter(history)(persistedReducer),
   composeWithDevTools(
     applyMiddleware(
       routerMiddleware(history),
@@ -22,3 +34,6 @@ export const store = createStore(
 );
 
 sagaMiddleware.run(rootSaga);
+
+export const PERSISTOR = persistStore(store);
+export const STORE = store;
