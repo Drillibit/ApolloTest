9bb3cac1399e2842af1ad7ccc282134b2bb38c1a
user <user@Mac15.local>
2018-08-23 09:40:06 +0700

    delete random film const in FrontPage

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -14,92 +14,6 @@ import { CONFIG } from '../services/api';
 
 /* eslint-disable */
 
-const somefilm = {
-  "adult": false,
-  "backdrop_path": "/55Vfc1beLn3pJF4Lw83TRKHenK6.jpg",
-  "belongs_to_collection": null,
-  "budget": 63000000,
-  "genres": [
-    {
-      "id": 18,
-      "name": "Drama"
-    }
-  ],
-  "homepage": "",
-  "id": 550,
-  "imdb_id": "tt0137523",
-  "original_language": "en",
-  "original_title": "Fight Club",
-  "overview": "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-  "popularity": 0.5,
-  "poster_path": null,
-  "production_companies": [
-    {
-      "id": 508,
-      "logo_path": "/7PzJdsLGlR7oW4J0J5Xcd0pHGRg.png",
-      "name": "Regency Enterprises",
-      "origin_country": "US"
-    },
-    {
-      "id": 711,
-      "logo_path": null,
-      "name": "Fox 2000 Pictures",
-      "origin_country": ""
-    },
-    {
-      "id": 20555,
-      "logo_path": null,
-      "name": "Taurus Film",
-      "origin_country": ""
-    },
-    {
-      "id": 54050,
-      "logo_path": null,
-      "name": "Linson Films",
-      "origin_country": ""
-    },
-    {
-      "id": 54051,
-      "logo_path": null,
-      "name": "Atman Entertainment",
-      "origin_country": ""
-    },
-    {
-      "id": 54052,
-      "logo_path": null,
-      "name": "Knickerbocker Films",
-      "origin_country": ""
-    },
-    {
-      "id": 25,
-      "logo_path": "/qZCc1lty5FzX30aOCVRBLzaVmcp.png",
-      "name": "20th Century Fox",
-      "origin_country": "US"
-    }
-  ],
-  "production_countries": [
-    {
-      "iso_3166_1": "US",
-      "name": "United States of America"
-    }
-  ],
-  "release_date": "1999-10-12",
-  "revenue": 100853753,
-  "runtime": 139,
-  "spoken_languages": [
-    {
-      "iso_639_1": "en",
-      "name": "English"
-    }
-  ],
-  "status": "Released",
-  "tagline": "How much can you know about yourself if you've never been in a fight?",
-  "title": "Fight Club",
-  "video": false,
-  "vote_average": 7.8,
-  "vote_count": 3439
-}
-
 const list = [
   { id: 356, name: 'Мультфильм' },
   { id: 357, name: 'Комедия' },
@@ -152,6 +66,24 @@ export class FrontPage extends Component {
 
   componentDidMount() {
     this.props.fetchNowPlaying();
+    //this.props.fetchOneMovie(this.randomFilm(this.props.filmsList.movies.sorted));
+  }
+
+  // static getDerivedStateFromProps(nextProps) {
+  //   console.log(nextProps, 'next');
+  //   // console.log(prevState, 'prev');
+    
+  //   if (nextProps.filmsList.movies.filmsList.length !== 0 && nextProps.filmsList.movies.filmsList.movie != 0) {
+  //     const rand = Math.floor(Math.random() * nextProps.filmsList.movies.filmsList.length);
+  //     console.log(rand);
+  //       nextProps.fetchOneMovie(nextProps.filmsList.movies.filmsList[rand]);
+  //   }
+  // }
+ 
+  randomFilm = arr => {
+    const rand = Math.floor(Math.random() * arr.length);
+    console.log(rand);
+    return arr[rand];
   }
 
   onScrollList = e => {
@@ -200,14 +132,14 @@ export class FrontPage extends Component {
   };
 
   render() {
-    console.log(this.props, 'props');
+    // console.log(this.props, 'props');
     const { fetchNowPlaying, fetchTop100, searchNowPlayingResults, filmsList, fetchByGenres } = this.props;
     const { top100Counter, nowPlayingCounter, isLoading } = this.state;
     return (
       <FrontPageStyled onScroll={this.onScrollList}>
         <FeaturedMovie film={filmsList.movies.movie} />
         <StyledGrid>
-        <button onClick={() => this.props.fetchOneMovie(33)}>click</button>
+        <button onClick={() => this.props.fetchOneMovie(this.randomFilm(this.props.filmsList.movies.sorted))}>click</button>
           <StyledRow>
             <StyledCol xs={12}>
               <Tabs onChange={id => (id === 0) 
