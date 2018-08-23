148a23371578d347ca126c219ff1d2e5930e9e58
Baranov Ivan <ibaranov@allmax.team>
2018-08-23 14:56:58 +0700

    first try

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,7 @@
 import { fork, all } from 'redux-saga/effects';
 
 import { sagas as movies } from './movies/sagas';
-import { sagas as genres } from './genres/sagas';
+import { fetchGenres as genres } from './genres/sagas';
 
 export function* rootSaga() {
   yield all([
