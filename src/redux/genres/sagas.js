3b7072f270e61f8ac55aa144fe6ddb6464b455ef
Baranov Ivan <ibaranov@allmax.team>
2018-08-27 17:37:38 +0700

    saga persist

--- src/redux/genres/sagas.js
+++ src/redux/genres/sagas.js
@@ -9,7 +9,6 @@ function* fetchGenres() {
   yield take('persist/REHYDRATE');
   const res = yield select(getGenres);
   if (res[12] === undefined) {
-    console.log('call')
     const { data } = yield call(requestFetchGenres);
     if (data && data.genres) {
       yield put(setGenres(data.genres));
