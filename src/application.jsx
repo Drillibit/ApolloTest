488e8c94ca4f24283df52ed28bbd5957fe6e953c
Baranov Ivan <ibaranov@allmax.team>
2018-08-31 18:00:30 +0700

    added apollo client

--- src/application.jsx
+++ src/application.jsx
@@ -6,6 +6,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHeart, faChevronUp, faChevronDown, faChevronLeft, faChevronRight, faCheck, faSearch, faPlay, faStar as faStarFill, faQuoteLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartFill, faStar } from '@fortawesome/free-regular-svg-icons';
 import { faFacebookF, faGooglePlusG, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 
 import { store, history } from './redux/store';
 import { Main } from './components/Main';
@@ -13,12 +15,16 @@ import { Main } from './components/Main';
 
 import './components/helpers/injectGlobalStyles';
 
+const client = ApolloClient({});
+
 export const Application = hot(module)(() => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Main />
-    </ConnectedRouter>
-  </Provider>
+  <ApolloProvider clinet={client}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Main />
+      </ConnectedRouter>
+    </Provider>
+  </ApolloProvider>
 ));
 
 library.add(
