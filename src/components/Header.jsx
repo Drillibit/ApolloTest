f0e5cc786c03ccc29006f15569bae6a5aed9c31a
Baranov Ivan <ibaranov@allmax.team>
2018-09-14 12:55:25 +0700

    @media setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,9 @@ const StyledHeaderContainer = styled.div`
     z-index: 1000;
     height: 109px;
     background-image: linear-gradient( to bottom,rgba(0,0,0,0.75) 0%,rgba(0,0,0,0) 100%);
+    @media (max-width: 440px) {
+    padding-top: 40px;
+  }
 `;
 
 const StyledRightGroup = styled.div`
@@ -26,22 +29,33 @@ const StyledRightGroup = styled.div`
   ${StyledButton} {
     padding: 0 36px;
   }
+  @media (max-width: 440px) {
+    position: relative;
+    padding-top: 1px;
+    flex-direction: column-reverse;
+    transform: scale(0.8);
+  }
 `;
 
 const SearchWrapper = styled.div`
   margin: 0 20px 0 auto;
+   @media (max-width: 440px) {
+    position: absolute;
+    top: 55px;
+    left: 10px;
+  }
 `;
 
 export const Header = () => (
   <StyledHeaderContainer>
     <StyledGrid>
       <StyledRow middle="xs">
-        <StyledCol xs={12} md={4} lg={6}>
+        <StyledCol xs={6} md={4} lg={6}>
         <Link to="/">
           <Logo />
         </Link>
         </StyledCol>
-        <StyledCol marginLeft="auto" xs={12} md={8} lg={6}>
+        <StyledCol marginLeft="auto" xs={6} md={8} lg={6}>
           <StyledRightGroup>
             <SearchWrapper>
               <SearchContainer />
