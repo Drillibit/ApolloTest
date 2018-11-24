99072386f2ee82e4b91d4af5823bd9484683b2ca
Drillibit <293344asd@gmail.com>
2018-11-24 14:39:33 +0700

    subscription client setup

--- src/application.jsx
+++ src/application.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-// import { HttpLink } from 'apollo-link-http';
-// import { onError } from 'apollo-link-error';
-// import { withClientState } from 'apollo-link-state';
-// import { ApolloLink, Observable } from 'apollo-link';
-import gql from 'graphql-tag';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { createUploadLink } from 'apollo-upload-client';
 import { Query, ApolloProvider } from 'react-apollo';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -40,15 +39,29 @@ import { Main } from './components/Main';
 import './components/helpers/injectGlobalStyles';
 import { Preloader } from './components/UIKit/Preloader';
 
+const ws = new SubscriptionClient('ws://localhost:3000/graphql', {
+  reconnect: true
+});
 
-const link = createUploadLink({
+const httpLink = createUploadLink({
   uri: '/graphql',
   credentials: 'include'
 });
 
+const wsLink = new WebSocketLink(ws);
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
+
+const cache = new InMemoryCache();
 const client = new ApolloClient({
   link,
-  cache: new InMemoryCache()
+  cache
 });
 
 export const Application = hot(module)(() => (
